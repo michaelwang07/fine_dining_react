afd7ea3cbb34f13f7a40a4163522efcc098cabf1
michaelwang07 <michaelwang07@yahoo.com>
2023-03-29 20:56:33 -0700

    laurels completion

--- src/components/Laurels/Laurels.jsx
+++ src/components/Laurels/Laurels.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './laurels.css'
+import { images } from '../../assets/exports/images'
+import data from '../../assets/exports/data'
+import Subheading from '../../templates/Subheading/Subheading'
+
+const AwardCard = ({ award: { imgUrl, title, subtitle }}) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="award" />
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{ color: '#DCCA87'}}>{title}</p>
+      <p className='p__cormorant' >{subtitle}</p>
+    </div>
+  </div>
+)
 
 const Laurels = () => {
   return (
-    <div>Laurels</div>
+    <div className='app__laurels app__wrapper app__bg section__padding' id="awards">
+      <div className='app__wrapper_info'>
+        <Subheading title='Awards & recognition' />
+        <h1 className='headtext__cormorant'>Our Laurels</h1>
+        <div className='app__laurels_awards'>
+          {data.awards.map((award) => (<AwardCard award={award} key={award.title}/>))}
+        </div>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.laurels} alt="laurels " />
+      </div>
+    </div>
   )
 }
 
